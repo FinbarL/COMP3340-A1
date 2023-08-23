aaea525e9966cf03b3ca07524d2579754ea4303a
FinbarL <113319479+FinbarL@users.noreply.github.com>
2023-08-24 09:24:44 +1000

    Now successfully getting all requsite parts

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 # Import Packages
 library(igraph)
+
 # Import the CSV Dataset
 dataset <- read.csv("USPresidency.csv")
 dataset_matrix <- as.matrix(dataset, nrow= nrow(data), ncol=ncol(data))
@@ -59,13 +60,36 @@ calc_jaccard_col <- function(input_matrix){
     }
     return (jaccard_measure_matrix)
 }
+
 cat("Formatted Matrix:\n")
 formatted_matrix
-cat("Hamming Distance Matrix (Row-wise):\n")
-calc_hamming_row(formatted_matrix)
-cat("Hamming Distance Matrix (Column-wise):\n")
-calc_hamming_col(formatted_matrix)
-cat("Jaccard Measure Matrix (Row-wise):\n")
-calc_jaccard_row(formatted_matrix)
-cat("Jaccard Measure Matrix (Column-wise):\n")
-calc_jaccard_col(formatted_matrix)
+
+cat("Ex. 1.a. Hamming Distance Matrix (Row-wise):\n")
+hamming_row <- calc_hamming_row(formatted_matrix)
+write.csv(hamming_row, file = "hamming_row.csv")
+hamming_row
+
+cat("Ex. 1.b. Hamming Distance Matrix (Column-wise):\n")
+hamming_col <- calc_hamming_col(formatted_matrix)
+write.csv(hamming_col, file = "hamming_col.csv")
+hamming_col
+
+cat("Ex. 1.a. Jaccard Measure Matrix (Row-wise):\n")
+jaccard_row <- calc_jaccard_row(formatted_matrix)
+write.csv(jaccard_row, file="jaccard_row.csv")
+jaccard_row
+
+cat("Ex 1.b. Jaccard Measure Matrix (Column-wise):\n")
+jaccard_col <- calc_jaccard_col(formatted_matrix)
+write.csv(jaccard_col, file="jaccard_col.csv")
+jaccard_col
+
+cat("Ex 1.c. MST for Hamming Distance Matrix (Row-wise):\n")
+g_hamming_row <- graph_from_adjacency_matrix(jaccard_row)
+hamming_row_mst <- mst(g_hamming_row)
+hamming_row_mst
+
+cat("Ex 1.c. MST for Hamming Distance Matrix (Column-wise):\n")
+g_hamming_col <- graph_from_adjacency_matrix(jaccard_row)
+hamming_col_mst <- mst(g_hamming_col)
+hamming_col_mst
