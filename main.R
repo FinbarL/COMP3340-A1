36369f1526f84810c6c24a8f55f4018f01bc9b3d
FinbarL <113319479+FinbarL@users.noreply.github.com>
2023-08-24 14:15:52 +1000

    Cleaning up code

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 # Import Packages
 library(igraph)
-
+library(cccd)
 # Import the CSV Dataset
 dataset <- read.csv("USPresidency.csv")
 dataset_matrix <- as.matrix(dataset, nrow= nrow(data), ncol=ncol(data))
@@ -11,8 +11,15 @@ dataset_matrix <- dataset_matrix[,colnames(dataset_matrix)!="Target"]
 formatted_matrix <- dataset_matrix[,-1]
 rownames(formatted_matrix) <- dataset_matrix[,1]
 
-
-
+get_formatted_matrix <- function(dataset) {
+    dataset_matrix <- as.matrix(dataset, nrow= nrow(data), ncol=ncol(data))
+    #remove the target column
+    dataset_matrix <- dataset_matrix[,colnames(dataset_matrix)!="Target"]
+    #make the row names the year column
+    formatted_matrix <- dataset_matrix[,-1]
+    rownames(formatted_matrix) <- dataset_matrix[,1]
+    return(formatted_matrix)
+}
 calc_hamming_dist <- function(A,B){
     return(sum(A != B))
 }
@@ -60,6 +67,20 @@ calc_jaccard_col <- function(input_matrix){
     }
     return (jaccard_measure_matrix)
 }
+calc_mst <- function(dist_matrix) {
+    graph <- graph.adjacency(as.matrix(dist_matrix), mode = "undirected", weighted = TRUE)
+    mst <- mst(graph)
+    return(mst)
+}
+calc_rng <- function(dist_matrix) {
+    rng_output <- rng(dist_matrix)
+    rng_output
+    return(rng_output)
+}
+get_graph_layout <- function(graph) {
+    layout <- layout_with_fr(graph)
+    return(layout)
+}
 
 cat("Formatted Matrix:\n")
 formatted_matrix
@@ -85,11 +106,43 @@ write.csv(jaccard_col, file="jaccard_col.csv")
 jaccard_col
 
 cat("Ex 1.c. MST for Hamming Distance Matrix (Row-wise):\n")
-g_hamming_row <- graph_from_adjacency_matrix(jaccard_row)
-hamming_row_mst <- mst(g_hamming_row)
-hamming_row_mst
+hamming_mst_row <- calc_mst(hamming_row)
+plot(hamming_mst_row, layout = get_graph_layout(hamming_mst_row),  main="MST for Hamming Distance Matrix (Row-wise)",)
 
 cat("Ex 1.c. MST for Hamming Distance Matrix (Column-wise):\n")
-g_hamming_col <- graph_from_adjacency_matrix(jaccard_row)
-hamming_col_mst <- mst(g_hamming_col)
-hamming_col_mst
+hamming_mst_col <- calc_mst(hamming_col)
+plot(hamming_mst_col, layout = get_graph_layout(hamming_mst_col), main="MST for Hamming Distance Matrix (Column-wise)",)
+
+cat("Ex 2. RNG for Hamming Distance Matrix (Row-wise):\n")
+hamming_rng_row <- calc_rng(hamming_row)
+plot(hamming_rng_row, layout = get_graph_layout(hamming_rng_row), main="RNG for Hamming Distance Matrix (Row-wise)",)
+
+cat("Ex 2. RNG for Hamming Distance Matrix (Column-wise):\n")
+hamming_rng_column <- calc_rng(hamming_col)
+plot(hamming_rng_column, layout = get_graph_layout(hamming_rng_column), main="RNG for Hamming Distance Matrix (Column-wise)",)
+
+cat("Ex 3. RNG for Jaccard Distance Matrix (Row-wise):\n")
+jaccard_rng_row <- calc_rng(jaccard_row)
+plot(jaccard_rng_row, layout = get_graph_layout(jaccard_rng_row), main="RNG for Jaccard Distance Matrix (Row-wise)",)
+
+cat("Ex 3. RNG for Jaccard Distance Matrix (Column-wise):\n")
+jaccard_rng_column <- calc_rng(jaccard_col)
+plot(jaccard_rng_column, layout = get_graph_layout(jaccard_rng_column),  main="RNG for Jaccard Distance Matrix (Column-wise)",)
+
+cat("Ex 4. MST for Jaccard Measure (Column-wise):\n")
+jaccard_mst_col <- calc_mst(jaccard_col)
+plot(jaccard_mst_col, layout = get_graph_layout(jaccard_rng_column),  main="MST for Jaccard Measure (Column-wise)",)
+
+cat("Ex 4. MST for Jaccard Measure (Row-wise):\n")
+jaccard_mst_row <- calc_mst(jaccard_row)
+plot(jaccard_mst_row, layout = get_graph_layout(jaccard_rng_column), main="MST for Jaccard Measure (Row-wise)",)
+
+cat("Ex. 5 Hamming Distance Matrix, k-NN graph with k=2 (row-wise)")
+hamming_knn_row <- nng(hamming_row, k=2)
+plot(hamming_knn_row, layout = get_graph_layout(jaccard_rng_column), main="k-NN graph with k=2 for Hamming Distance Matrix, (row-wise)")
+
+cat("Ex. 6 Hamming Distance Matrix, k-NN graph with k=2 (column-wise)")
+hamming_knn_col <- nng(hamming_col, k=2)
+plot(hamming_knn_col, layout = get_graph_layout(jaccard_rng_column), main="k-NN graph with k=2 for Hamming Distance Matrix, (column-wise)")
+
+
